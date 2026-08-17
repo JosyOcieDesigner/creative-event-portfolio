a5ae662eecc5ebb0aaf00c4d5f672650ebe285ef
JosyOcieDesigner <josyocie@gmail.com>
2026-08-17 19:03:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -111,7 +111,9 @@ if (grid) {
 if (!v.hidePlay) {
   button.append(play);
 }
-
+button.addEventListener("click", () => {
+  playInline(card, button, v);
+});
     iframe.src =
   `https://www.youtube-nocookie.com/embed/${video.id}` +
   `?autoplay=1&controls=1&rel=0&modestbranding=1&iv_load_policy=3&playsinline=1`;
