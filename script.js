69b8add89e47e2596a30c8e2d0a5285e3f9ce2b8
JosyOcieDesigner <josyocie@gmail.com>
2026-08-18 20:08:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -140,8 +140,8 @@ function playInline(card, button, video) {
   const iframe = document.createElement("iframe");
 
   iframe.src =
-    `https://www.youtube-nocookie.com/embed/${video.id}` +
-    `?autoplay=1&controls=1&rel=0&modestbranding=1&iv_load_policy=3&playsinline=1`;
+  `https://www.youtube-nocookie.com/embed/${video.id}` +
+  `?autoplay=1&mute=1&controls=1&rel=0&modestbranding=1&iv_load_policy=3&playsinline=1`;
 
   iframe.title = video.title;
   iframe.allow = "autoplay; encrypted-media; picture-in-picture";
